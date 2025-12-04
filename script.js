3edb6ea405b0e45bf970f6a6fb7bd4de08e3f906
Luke <slukehall92@gmail.com>
2025-12-04 13:55:22 -0500

    Add basic buttons Add basic mult, div, subtract, add functions Create 3 Var for input 1, input 2, and operator

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a +b 
+};
+
+function subtract(a,b){
+    return a-b
+};
+
+function mult(a,b){
+    return a*b
+};
+
+function div(a,b){
+    return a/b
+};
+
+let num1 = "";
+let num2 = "";
+let operator = "";
