6f5de8f50aa7cc743f122dd291140f7af1221dbf
Luke <slukehall92@gmail.com>
2025-12-04 14:00:08 -0500

    Saved and reuploaded

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ function div(a,b){
 let num1 = "";
 let num2 = "";
 let operator = "";
+
+
+function operate(num1,num2,operator){
+
+}
+
+
+//Takes user num1, operator, and num2. Somehow must run the functions. So maybe 
+// take the operator and somehow 'link' the functions to the variable. But have 
+// to find a way to only accept those four functions and nothing else.
