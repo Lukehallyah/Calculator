35f5c5cf761154fdd12cab7fb39c350866a2d2b3
Luke <slukehall92@gmail.com>
2025-12-07 14:16:32 -0500

    Basic Logic Worked Out

--- script.js
+++ script.js
@@ -14,16 +14,43 @@ function div(a,b){
     return a/b
 };
 
-let num1 = "";
-let num2 = "";
-let operator = "";
-
-
-function operate(num1,num2,operator){
-
+function operator(op){
+    num1=inputField.innerHTML;
+    oper=op;
+    inputField.innerHTML='';
 }
 
 
-//Takes user num1, operator, and num2. Somehow must run the functions. So maybe 
-// take the operator and somehow 'link' the functions to the variable. But have 
-// to find a way to only accept those four functions and nothing else.
+function equals(){
+    num2=inputField.innerHTML;
+    let a= parseInt(num1);
+    let b= parseInt(num2);
+    let result;
+
+    if(oper==="+") result= add(a,b);
+    else if(oper==="-") result= subtract(a,b);
+    else if(oper==="*") result= mult(a,b);
+    else if(oper==="/") result= div(a,b);
+
+    inputField.innerHTML=result;
+}
+
+
+//the function: has to put multiple values into a string
+//   and then parseInt() so that it becomes a number.
+let inputField = document.querySelector('.inputField');
+let clear = document.querySelector('.clear');
+
+function showNumber(num){
+    let current ="";
+    current+=num;
+    document.querySelector(".inputField").innerHTML+=current;
+}
+
+
+function clearBox(){
+    return inputField.innerHTML="";
+}
+//Now it puts the number there but I must find a way to 
+// make it where it add it to like a string or somethind
+// and parses it.
